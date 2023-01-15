68725bce6a028285778cd340d572879479e7eb43
Richard Dong <66289396+rdong8@users.noreply.github.com>
2023-01-15 04:11:40 +0000

    Added Ansible logging

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 function setup() {
-  # Exit if there is an error
-  set -e  
-
-  # This will error if the directory already exists, preventing this script from being run twice without the user knowing
-  sudo mkdir /var/log/impulse/ 
-
   sudo apt-get update
   sudo apt-get install -y ansible
 
+  ANSIBLE_LOG="ANSIBLE_LOG_PATH=/var/log/impulse/ansible.log"
+  PULL="ansible-pull -U ssh://git@github.com:rdong8/impulse-cfg.git |& sudo tee -a /var/log/impulse/impulse-pull.log"
   echo "\n# Start from impulse-cfg setup.sh" >> ~/.bashrc
-  echo "alias impulse-pull=\"ansible-pull -U ssh://git@github.com:rdong8/impulse-cfg.git |& sudo tee -a /var/log/impulse/impulse-pull.log\"" >> ~/.bashrc
-  echo "# End impulse-cfg setup.sh" >> ~/.bashrc
+  echo "$ANSIBLE_LOG alias impulse-pull=\"$PULL\"" >> ~/.bashrc
+  echo "# End impulse-cfg setup.sh\n" >> ~/.bashrc
   source ~/.bashrc
   sudo impulse-pull
 }
+
+# Exit if there is an error
+set -e  
+
+# Errors if directory already exists, preventing script from accidentally being run twice
+sudo mkdir /var/log/impulse/ 
+sudo touch /var/log/impulse/setup.log
 
 setup |& tee -a /var/log/impulse/setup.log
